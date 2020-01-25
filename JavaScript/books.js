8685bd117af414efa312aa451bae4c87f170eae5
Laxmikant Nirmohi <laxmikant.nirmohi@gmail.com>
2020-01-25 14:12:19 +0530

    add submission event, fucntion to add row to table and render the table

--- JavaScript/books.js
+++ JavaScript/books.js
@@ -1,5 +1,9 @@
 let myLibrary = [];
 
+const form = document.getElementById("form");
+
+const tableBody = document.getElementById("tbody");
+
 function books(title, author, noOfPages, isBookRead) {
 
     this.title = title;
@@ -12,10 +16,87 @@ function books(title, author, noOfPages, isBookRead) {
     };
 }
 
-function addBookToLibrary(book) {
-    myLibrary.push(book);
+form.onsubmit = (event) => {
+
+    event.preventDefault();
+
+    processFormData([...event.target.elements]);
+
+    //console.log(event.target.elements);
+};
+
+function processFormData(formValues) {
+
+    let valueArr = [];
+
+    label:
+    for(let i = 0; i < formValues.length; i++) {
+
+        let element = formValues[i];
+
+        if(element.nodeName === "BUTTON" ||
+            (element.type === "radio" && element.checked === false)
+        ) {
+            continue label;
+        }
+
+        valueArr.push(element.value);
+    }
+
+    //console.log(valueArr);
+
+    const tableRow = createTableRowForBook(valueArr, myLibrary.length);
+
+    addBookToTable(tableRow);
+
+    addBookToLibrary(new books(...valueArr));
+}
+
+
+function addBookToLibrary(book, index = null) {
+    
+    if(index !== null) {
+        myLibrary.splice(index, 0, book);
+    }else {
+        myLibrary.push(book);
+    }
+
+    console.table(myLibrary);
+}
+
+function addBookToTable(book) {
+
+    tableBody.appendChild(book);
 }
 
 function render() {
     
+    for (let index = 0; index < myLibrary.length; index++) {
+
+        let [name, author, pages, status] = [...myLibrary[i]];
+        
+        createTableRowForBook(myLibrary[i], index);
+    }
+}
+
+function createTableRowForBook(valuesForRow, index) {
+
+    const tr = document.createElement("tr");
+
+    tr.setAttribute("data-index", index);
+
+    tr.textContent = index;
+
+    for(let i = 0; i < valuesForRow.length;i++) {
+
+        let value = valuesForRow[i];
+
+        const td = document.createElement("td");
+
+        td.textContent = value;
+
+        tr.appendChild(td);
+    }
+
+    return tr;
 }
